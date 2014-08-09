678918736650673c5ad5a38b164ae6d59ac1c957
Corey Ellis <corey@coreyellis.me>
2014-08-09 15:45:15 -0500

    basic foundation scss, js and code kit config

--- root/js/src/theme.js
+++ root/js/src/theme.js
@@ -5,6 +5,30 @@
  * Copyright (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
  * Licensed under the GPLv2+ license.
  */
+
+/* Foundation Scripts */
+//@codekit-prepend "../vendor/foundation.js";
+//@codekit-prepend "../vendor/foundation/foundation.abide.js";
+//@codekit-prepend "../vendor/foundation/foundation.accordion.js";
+//@codekit-prepend "../vendor/foundation/foundation.alert.js";
+//@codekit-prepend "../vendor/foundation/foundation.clearing.js";
+//@codekit-prepend "../vendor/foundation/foundation.dropdown.js";
+//@codekit-prepend "../vendor/foundation/foundation.equalizer.js";
+//@codekit-prepend "../vendor/foundation/foundation.interchange.js";
+//@codekit-prepend "../vendor/foundation/foundation.joyride.js";
+//@codekit-prepend "../vendor/foundation/foundation.magellan.js";
+//@codekit-prepend "../vendor/foundation/foundation.offcanvas.js";
+//@codekit-prepend "../vendor/foundation/foundation.orbit.js";
+//@codekit-prepend "../vendor/foundation/foundation.reveal.js";
+//@codekit-prepend "../vendor/foundation/foundation.slider.js";
+//@codekit-prepend "../vendor/foundation/foundation.tab.js";
+//@codekit-prepend "../vendor/foundation/foundation.tooltip.js";
+//@codekit-prepend "../vendor/foundation/foundation.topbar.js";
+
+/* Other global scripts */
+//@codekit-prepend "../vendor/fastclick.js";
+
+jQuery(document).foundation(); 
  
 (function (window, $, undefined) {
 	'use strict';
